d3b47f9f80d742f92667879091cd3a7a5f01d838
Javier Jimenez <javierrcc522@gmail.com>
2017-09-15 11:45:57 -0700

    add on the app

--- app.rb
+++ app.rb
@@ -8,21 +8,20 @@ require('pry')
 
 get('/') do
   @list = WordDefiner.all()
-
   erb(:list)
-end
-
-get('/define/:id') do
-  @define = WordDefiner.find(params[:id])
-
-  erb(:contact)
 end
 
 post('/') do
   define_word = params['define_word']
   word_meaning = params['word_meaning']
-
   word_being_define = {'define_word' => define_word, 'word_meaning' => word_meaning}
+  define = WordDefiner.new(word_being_define)
+  define.save()
+  erb(:list)
+end
 
+get('/update/:id') do
+  @define = WordDefiner.find(params[:id])
 
+  erb(:update)
 end
