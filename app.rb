5c8bab91eae25d06fc1d51433bdeda027bc188dd
Javier Jimenez <javierrcc522@gmail.com>
2017-09-15 09:50:17 -0700

    add a initialize method with variable instances also app.rb with a GET and POST request

---
+++ app.rb
@@ -0,0 +1,28 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/word_definer')
+require('pry')
+
+
+
+get('/') do
+  @list = WordDefiner.all()
+
+  erb(:list)
+end
+
+get('/define/:id') do
+  @define = WordDefiner.find(params[:id])
+
+  erb(:contact)
+end
+
+post('/') do
+  define_word = params['define_word']
+  word_meaning = params['word_meaning']
+
+  word_being_define = {'define_word' => define_word, 'word_meaning' => word_meaning}
+
+  define =
+end
---
+++ lib/word_definer.rb
@@ -0,0 +1,16 @@
+class WordDefiner
+  @@list = []
+  attr_accessor :define_word, :word_meaning
+  attr_reader :id
+
+  def initialize(attributes)
+    @define_word = attributes.fetch('define_word')
+    @word_meaning = attributes.fetch('word_meaning')
+    @id = @@list.length + 1
+  end
+
+  def self.all
+    @@list
+  end
+
+end
