dd8bac075e65d9eaddf60d363dd868f5b5270e71
Javier Jimenez <javierrcc522@gmail.com>
2017-09-19 22:52:04 -0700

    add three more integrated passing tests

--- spec/word_definer_integration_spec.rb
+++ spec/word_definer_integration_spec.rb
@@ -3,21 +3,49 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-  describe('my define word', {:type => :feature}) do
+describe("WordDefiner") do
+  before() do
+      WordDefiner.clear()
+    end
+
+  describe('puts my define word in the list', {:type => :feature}) do
     it ('processes the user entry and returns a list of words') do
       visit('/')
-      fill_in('define_word', :with => 'margaret')
+      fill_in('define_word', :with => 'mango')
       click_button('add word')
-      expect(page).to have_content('margaret')
+      expect(page).to have_content('mango')
     end
   end
 
+  describe('click on the define words', {:type => :feature}) do
+    it ('processes the user entry where a user can click on the link of the difine word') do
+      visit('/')
+      fill_in('define_word', :with => 'monitor')
+      click_button('add word')
+      expect(page).to have_content('monitor')
+    end
+  end
 
-  # describe('a contact', {:type => :feature}) do
-  #   it ('goes to the detail page') do
-  #     visit('/')
-  #     save_and_open_page
-  #     click_link("kitten margaret")
-  #     expect(page).to have_content('margaret')
-  #   end
-  # end
+  describe('it removes a define word', {:type => :feature}) do
+    it ('processes the user entry where a user can click button and it will delete the define word') do
+      visit('/')
+      fill_in('define_word', :with => 'macbook')
+      click_button('add word')
+      click_link('macbook')
+      click_button('delete word')
+      expect(page).to have_no_content('macbook')
+    end
+  end
+
+  describe('it adds multiple word defines', {:type => :feature}) do
+    it ('processes the user entry where a user can click on the Add word button and add more words') do
+      visit('/')
+      fill_in('define_word', :with => 'cats')
+      click_button('add word')
+      fill_in('define_word', :with => 'dogs')
+      click_button('add word')
+      expect(page).to have_content('cats', 'dogs')
+    end
+  end
+
+end
